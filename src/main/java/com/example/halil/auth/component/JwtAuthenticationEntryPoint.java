e6ebf4d5c0ac8ceeb61fe8d6d9ba1ffd1ad4a7cc
SeongHyeon Ji <j.eq.h0507@gmail.com>
2025-05-24 22:07:15 +0900

    refactor: JwtAuthenticationEntryPoint 에러 처리 방식 리팩토링
    기존 하드코딩된 response body 부분을 jackson ObjectMapper로 직렬화하도록 변경

--- src/main/java/com/example/halil/auth/component/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/halil/auth/component/JwtAuthenticationEntryPoint.java
@@ -1,8 +1,12 @@
 package com.example.halil.auth.component;
 
+import com.example.halil.auth.exception.AuthErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
@@ -10,8 +14,11 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(
@@ -26,16 +33,16 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         String ua = request.getHeader("User-Agent");
         int httpStatusCode = HttpStatus.UNAUTHORIZED.value();
 
-        log.warn("[event=authentication_failure] [status={}] [path={}] [method={}] [ip={}] [ua={}]",
-                httpStatusCode, path, method, ip, ua);
+        log.warn("[event=authentication_failure] [status={}] [path={}] [method={}] [ip={}] [user-agent={}]",
+                httpStatusCode, path, method, ip, ua
+        );
+
+        AuthErrorResponse authErrorResponse = new AuthErrorResponse(
+                httpStatusCode, authException.getMessage(), LocalDateTime.now(), path
+        );
 
         response.setContentType("application/json");
         response.setStatus(httpStatusCode);
-        response.getWriter().write(String.format("""
-                {
-                    "message":"token is missing or invalid",
-                    "statusCode":%s
-                }
-                """, httpStatusCode));
+        response.getWriter().write(objectMapper.writeValueAsString(authErrorResponse));
     }
 }
